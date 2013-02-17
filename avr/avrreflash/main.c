58201eb946cb57d3dfe3c5b7c2efadde2f2f59b6
vedran <vedranb@gmail.com>
2013-02-17 02:37:07 -0800

    added simple monkey patching

--- avr/avrreflash/main.c
+++ avr/avrreflash/main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "uart.h"
 
@@ -14,8 +15,9 @@
 #define LED_SMALL_BIT (1<<PORTB0)
 #define LED_BIG_BIT (1<<PORTB1)
 
-uint8_t led_big_asm[PAGE_SIZE];
+uint8_t instruction_buffer[PAGE_SIZE];
 uint8_t led_small_asm[PAGE_SIZE];
+uint8_t led_big_asm[PAGE_SIZE];
 
 void flash_write(uint16_t start, uint8_t * page) BOOTLOADER_SECTION;
 void flash_write(uint16_t start, uint8_t * page)
@@ -47,13 +49,26 @@ void flash_write(uint16_t start, uint8_t * page)
     sei();
 }
 
-void led_on(void) __attribute__ ((section (".led")));
-void led_on() {
-    LED_PORT |= LED_PORT;
+int char_to_hex(char c) {
+    return isdigit(c) ? c - '0' : tolower(c) - 'a' + 0xA;
+}
+
+void str_to_hex(char *input, uint8_t* output) {
+    while(*input) {
+        *output++ = (char_to_hex(input[0])<<4) | char_to_hex(input[1]);
+        input += 2;
+    }
+}
+
+void flashed(void) __attribute__ ((section (FLASH_SECTION)));
+void flashed() {
+    LED_PORT |= LED_PORT; // dummy operation
 }
 
 int main(void)
 {
+    memset(instruction_buffer, 0xff, PAGE_SIZE);
+    /* old LED test
     memset(led_big_asm, 0xff, PAGE_SIZE);
     memset(led_small_asm, 0xff, PAGE_SIZE);
 
@@ -66,6 +81,7 @@ int main(void)
     led_small_asm[1] = 0x9a;
     led_small_asm[2] = 0x08;
     led_small_asm[3] = 0x95;
+    */
 
     LED_DDR |= (1<<PORTB0);
     LED_DDR |= (1<<PORTB1);
@@ -76,23 +92,24 @@ int main(void)
     uart_init(BAUD(9600));
     printf("system start\n");
 
-    char buf[128];
+    char buf[512];
+
+    /* sample commands
+     * return: 08 95
+     * turn on small LED (and return):  28 9a 08 95
+     * turn on big LED (and return):    29 9a 08 95
+     * turn off both LEDs (and return): 15 b8 08 95 */
 
     for(;;){
         printf("> ");
-        fgets(buf,128,stdin);
+        fgets(buf,PAGE_SIZE,stdin);
         if (strlen(buf) && buf[strlen(buf)-1] == '\n'){
             buf[strlen(buf)-1] = '\0';
-            if(strcmp(buf, "big") == 0) {
-                LED_PORT = 0;
-                flash_write(0x1000, led_big_asm);
-                led_on();
-            }
-            else if(strcmp(buf, "small") == 0) {
-                LED_PORT = 0;
-                flash_write(0x1000, led_small_asm);
-                led_on();
-            }
+            str_to_hex(buf, instruction_buffer);
+            flash_write(0x1000, instruction_buffer);
+            flashed();
+            memset(buf, '\0', PAGE_SIZE);
+            memset(instruction_buffer, 0xff, PAGE_SIZE);
         }
     }
 }
