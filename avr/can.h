ea9bbe657b83233be8eb830e9661d3330fa7b737
Andrew Fuller <qartis@gmail.com>
2013-08-03 01:49:39 -0700

    move type and id lists into can.h

---
+++ avr/can.h
@@ -0,0 +1,86 @@
+#define TYPE_LIST(X) \
+    X(value_periodic, 0) \
+    X(value_explicit, 1) \
+    X(set_time, 2) \
+    X(set_interval, 3) \
+    X(sos_reboot, 4) \
+    X(sos_rx_overrun, 5) \
+    X(sos_stfu, 6) \
+    X(sos_nostfu, 7) \
+\
+    X(file_read, 0x10) \
+    X(file_write, 0x11) \
+    X(file_error, 0x12) \
+    X(file_tree, 0x13) \
+    X(file_header, 0x14) \
+\
+    X(sensor_error, 0x13) \
+\
+\
+    X(file_checksum, 0x15) \
+\
+\
+    X(get_arm, 0x20) \
+    X(set_arm, 0x21) \
+\
+    X(get_output, 0x22) \
+    X(set_output, 0x23) \
+\
+    X(get_value, 0x24) \
+    X(set_value, 0x25) \
+\
+    X(xfer_cts, 0x80) \
+    X(xfer_chunk, 0x81) \
+    X(xfer_cancel, 0x82) \
+\
+    X(modema_at_0, 0x90) \
+    X(modema_at_1, 0x91) \
+    X(modema_at_2, 0x92) \
+    X(modema_at_3, 0x93) \
+    X(modema_at_4, 0x94) \
+    X(modema_at_5, 0x95) \
+    X(modema_at_6, 0x96) \
+    X(modema_at_7, 0x97) \
+\
+    X(get_unread_logs, 0xa0) \
+\
+    X(invalid, 0xff) \
+
+
+#define ID_LIST(X) \
+    X(any,     0x00) \
+    X(ping,    0x01) \
+    X(pong,    0x02) \
+    X(laser,   0x03) \
+    X(gps,     0x04) \
+    X(temp,    0x05) \
+    X(time,    0x06) \
+    X(sd,      0x07) \
+    X(arm,     0x08) \
+    X(heater,  0x09) \
+    X(lidar,   0x0a) \
+    X(powershot, 0x0b) \
+    X(9dof,    0x0c) \
+    X(compass, 0x0d) \
+    X(modema,  0x0e) \
+    X(modemb,  0x0f) \
+    X(accel,   0x10) \
+    X(gyro,    0x11) \
+    X(invalid, 0x1f) \
+
+
+enum type {
+#define X(name, value) TYPE_ ## name = value, \
+
+    TYPE_LIST(X)
+#undef X
+};
+
+
+enum id {
+#define X(name, value) ID_ ## name = value, \
+
+    ID_LIST(X)
+#undef X
+};
+
--- avr/mcp2515.h
+++ avr/mcp2515.h
@@ -1,86 +1,3 @@
-#define TYPE_LIST(X) \
-    X(value_periodic, 0) \
-    X(value_explicit, 1) \
-    X(set_time, 2) \
-    X(set_interval, 3) \
-    X(sos_reboot, 4) \
-    X(sos_rx_overrun, 5) \
-    X(sos_stfu, 6) \
-    X(sos_nostfu, 7) \
-\
-    X(file_read, 0x10) \
-    X(file_write, 0x11) \
-    X(file_error, 0x12) \
-    X(file_tree, 0x13) \
-\
-    X(sensor_error, 0x13) \
-\
-\
-    X(file_checksum, 0x15) \
-\
-\
-    X(get_arm, 0x20) \
-    X(set_arm, 0x21) \
-\
-    X(get_output, 0x22) \
-    X(set_output, 0x23) \
-\
-    X(get_value, 0x24) \
-    X(set_value, 0x25) \
-\
-    X(xfer_cts, 0x80) \
-    X(xfer_chunk, 0x81) \
-    X(xfer_cancel, 0x82) \
-\
-    X(modema_at_0, 0x90) \
-    X(modema_at_1, 0x91) \
-    X(modema_at_2, 0x92) \
-    X(modema_at_3, 0x93) \
-    X(modema_at_4, 0x94) \
-    X(modema_at_5, 0x95) \
-    X(modema_at_6, 0x96) \
-    X(modema_at_7, 0x97) \
-\
-    X(invalid, 0xff) \
-
-
-#define ID_LIST(X) \
-    X(any,     0x00) \
-    X(ping,    0x01) \
-    X(pong,    0x02) \
-    X(laser,   0x03) \
-    X(gps,     0x04) \
-    X(temp,    0x05) \
-    X(time,    0x06) \
-    X(sd,      0x07) \
-    X(arm,     0x08) \
-    X(heater,  0x09) \
-    X(lidar,   0x0a) \
-    X(powershot, 0x0b) \
-    X(9dof,    0x0c) \
-    X(compass, 0x0d) \
-    X(modema,  0x0e) \
-    X(modemb,  0x0f) \
-    X(accel,   0x10) \
-    X(gyro,    0x11) \
-    X(invalid, 0x1f) \
-
-
-enum type {
-#define X(name, value) TYPE_ ## name = value, \
-
-    TYPE_LIST(X)
-#undef X
-};
-
-
-enum id {
-#define X(name, value) ID_ ## name = value, \
-
-    ID_LIST(X)
-#undef X
-};
-
 typedef uint8_t mcp2515_id_t;
 typedef uint8_t mcp2515_type_t;
 
@@ -270,10 +187,10 @@ void mcp2515_reset(void);
 uint8_t read_register(uint8_t address);
 void write_register(uint8_t address, uint8_t value);
 void modify_register(uint8_t address, uint8_t mask, const uint8_t value);
-uint8_t mcp2515_send(uint8_t type, uint8_t id, uint8_t len, const void *data);
+uint8_t mcp2515_send(uint8_t type, uint8_t id, const void *data, uint8_t len);
 uint8_t mcp2515_send2(struct mcp2515_packet_t *p);
-void load_tx0(uint8_t type, uint8_t id, uint8_t len, const uint8_t *data);
+void load_tx0(uint8_t type, uint8_t id, const uint8_t *data, uint8_t len);
 
-uint8_t mcp2515_xfer(uint8_t type, uint8_t dest, uint8_t len, void *data);
+uint8_t mcp2515_xfer(uint8_t type, uint8_t dest, const void *data, uint8_t len);
 uint8_t mcp2515_receive_xfer_wait(uint8_t type, uint8_t sender_id,
     mcp2515_xfer_callback_t xfer_cb);
