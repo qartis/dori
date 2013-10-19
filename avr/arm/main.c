ece64239f3b22b692e770a65d264a7d6311044b3
vedran <vedranb@gmail.com>
2013-10-19 02:58:26 -0700

    Updated arm and stepper CAN driver

--- avr/arm/main.c
+++ avr/arm/main.c
@@ -20,30 +20,11 @@
 
 uint8_t uart_irq(void)
 {
-    int32_t i;
-    int32_t goal;
-    scanf("%ld", &goal);
-    if (goal > 9) {
-        for (i = 0; i < goal; i++) {
-            stepper_cw();
-            _delay_us(200);
-            _delay_ms(1);
-        }
-    } else if (goal < 0) {
-        for (i = goal; i < 0; i++) {
-            stepper_ccw();
-            _delay_us(200);
-            _delay_ms(1);
-        }
-    } else {
-        set_arm_angle(goal);
-    }
     return 0;
 }
 
-uint8_t periodic_irq(void)
+uint8_t send_arm_angle(uint8_t type)
 {
-    /* this also gets called when we move the arm */
     uint8_t rc;
     uint16_t angle;
     
@@ -58,42 +39,106 @@ uint8_t periodic_irq(void)
 
     counter++;
 
-    //rc = mcp2515_send(TYPE_value_periodic, ID_arm, buf, 3);
-    rc = 0;
+    rc = mcp2515_send_tagged(type, ID_arm, buf, 3, TAG_get_arm);
     if (rc != 0) {
-        return rc;
+        puts_P(PSTR("err: arm: mcp2515_send_tagged"));
     }
 
-    return 0;
+    return rc;
 }
 
-uint8_t can_irq(void)
+uint8_t send_stepper_angle(uint8_t type)
 {
-    uint8_t pos;
+    uint8_t rc;
+    uint16_t angle;
+    
+    angle = get_stepper_angle();
 
-    packet.unread = 0;
+    uint8_t buf[3];
+    static uint8_t counter = 0;
 
-    switch (packet.type) {
-    case TYPE_set_arm:
-        if (packet.len != 1) {
-            puts_P(PSTR("err: TYPE_set_arm data len"));
-            break;
-        }
-        pos = packet.data[0];
-        set_arm_angle(pos);
-        /* send arm angle again */
-        periodic_irq();
+    buf[0] = angle >> 8;
+    buf[1] = angle & 0xff;
+    buf[2] = counter;
+
+    counter++;
+
+    rc = mcp2515_send_tagged(type, ID_arm, buf, 3, TAG_get_stepper);
+    if (rc != 0) {
+        puts_P(PSTR("err: arm: mcp2515_send_tagged"));
+    }
+
+    return rc;
+}
+
+
+uint8_t periodic_irq(void)
+{
+    uint8_t rc = send_arm_angle(TYPE_value_periodic);
+    if(rc != 0) return rc;
+
+    rc = send_stepper_angle(TYPE_value_periodic);
+    return rc;
+}
+
+uint8_t process_value_request(void)
+{
+    uint8_t rc = 0;
+
+    switch(packet.tag) {
+    case TAG_get_arm:
+        rc = send_arm_angle(TYPE_value_explicit);
         break;
-
-    case TYPE_get_arm:
-        periodic_irq();
-        break;
-
+    case TAG_get_stepper:
+        rc = send_stepper_angle(TYPE_value_explicit);
     default:
         break;
     }
 
-    return 0;
+    return rc;
+}
+
+uint8_t process_command(void)
+{
+    uint8_t rc = 0;
+
+    switch(packet.tag) {
+    case TAG_set_arm:
+        {
+            uint8_t val = packet.data[0];
+            set_arm_percent(val);
+            break;
+        }
+    case TAG_set_stepper:
+        {
+            uint16_t val = packet.data[0] << 8;
+            val |= packet.data[1] & 0xff;
+            set_stepper_angle(val);
+            break;
+        }
+    default:
+        break;
+    }
+
+    return rc;
+}
+
+
+uint8_t can_irq(void)
+{
+    uint8_t rc = 0;
+
+    switch (packet.type) {
+    case TYPE_value_request:
+        rc = process_value_request();
+    case TYPE_command:
+        rc = process_command();
+    default:
+        break;
+    }
+
+    packet.unread = 0;
+    return rc;
 }
 
 void main(void)
--- can.h
+++ can.h
@@ -1,50 +1,26 @@
 #define TYPE_LIST(X) \
-    X(nop, 0) \
+    X(nop, 0x00) \
 \
-    X(value_periodic, 1) \
-    X(value_explicit, 2) \
-    X(value_request, 3) \
-    X(set_time, 4) \
-    X(set_interval, 5) \
-    X(sos_reboot, 6) \
-    X(sos_rx_overrun, 7) \
-    X(sos_stfu, 8) \
-    X(sos_nostfu, 9) \
+    X(command, 0x01) \
+    X(value_request, 0x02) \
+    X(value_periodic, 0x03) \
+    X(value_explicit, 0x04) \
 \
-    X(file_read, 0x10) \
-    X(file_write, 0x11) \
-    X(file_error, 0x12) \
-    X(file_tree, 0x13) \
-    X(dcim_read, 0x14) \
-    X(dcim_header, 0x15) \
-    X(file_header, 0x16) \
-    X(disk_full, 0x17) \
-    X(dcim_len, 0x18) \
-    X(file_offer, 0x19) \
+    X(set_time, 0x0a) \
+    X(set_interval, 0x0b) \
+    X(sos_reboot, 0x0c) \
+    X(sos_rx_overrun, 0x0d) \
+    X(sos_stfu, 0x0e) \
+    X(sos_nostfu, 0x0f) \
+    X(sensor_error, 0x10) \
+    X(format_error, 0x11) \
 \
-    X(get_arm, 0x20) \
-    X(set_arm, 0x21) \
+    X(file_offer, 0x20) \
+    X(file_checksum, 0x21) \
 \
-    X(sensor_error, 0x1a) \
-    X(file_checksum, 0x1b) \
-\
-    X(xfer_cts, 0x80) \
-    X(xfer_chunk, 0x81) \
-    X(xfer_cancel, 0x82) \
-\
-    X(at_0_write, 0x90) \
-    X(at_1_write, 0x91) \
-    X(at_2_write, 0x92) \
-    X(at_3_write, 0x93) \
-    X(at_4_write, 0x94) \
-    X(at_5_write, 0x95) \
-    X(at_6_write, 0x96) \
-    X(at_7_write, 0x97) \
-    X(at_send,    0x98) \
-\
-    X(get_unread_logs, 0xa0) \
-\
-    X(format_error, 0xb0) \
+    X(xfer_cts, 0x30) \
+    X(xfer_chunk, 0x31) \
+    X(xfer_cancel, 0x32) \
 \
     X(invalid, 0xff) \
 
@@ -70,6 +46,41 @@
     X(enviro,  0x12) \
     X(invalid, 0x1f) \
 
+// Tags are used for defining the specific
+// scope of a command or value_* type message
+// i.e. a type of value_periodic with a tag of temp
+// or a command with tag get_arm
+#define TAG_LIST(X) \
+    X(file_read, 0x00) \
+    X(file_write, 0x01) \
+    X(file_error, 0x02) \
+    X(file_tree, 0x03) \
+    X(dcim_read, 0x04) \
+    X(dcim_header, 0x05) \
+    X(file_header, 0x06) \
+    X(disk_full, 0x07) \
+    X(dcim_len, 0x08) \
+\
+    X(at_0_write, 0x00) \
+    X(at_1_write, 0x01) \
+    X(at_2_write, 0x02) \
+    X(at_3_write, 0x03) \
+    X(at_4_write, 0x04) \
+    X(at_5_write, 0x05) \
+    X(at_6_write, 0x06) \
+    X(at_7_write, 0x07) \
+    X(at_send,    0x08) \
+\
+    X(get_unread_logs, 0x00) \
+\
+    X(get_arm, 0x00) \
+    X(set_arm, 0x01) \
+    X(get_stepper, 0x02) \
+    X(set_stepper, 0x03) \
+\
+    X(laser_dist, 0x00) \
+\
+
 
 enum type {
 #define X(name, value) TYPE_ ## name = value, \
@@ -86,3 +97,10 @@ enum id {
 #undef X
 };
 
+
+enum tag {
+#define X(name, value) TAG_ ## name = value, \
+
+    TAG_LIST(X)
+#undef X
+};
