89558d5affe36182f3b7eced98b0ed039dc440e9
Andrew Fuller <qartis@gmail.com>
2014-02-15 12:17:43 -0800

    move internal ut390 laser definitions to separate header

---
+++ avr/arm/laser_defs.h
@@ -0,0 +1,37 @@
+#define ON_BTN  DDRD, PORTD, PIND6
+#define OFF_BTN DDRD, PORTD, PIND3
+
+#define VPLUS_PIN PIND
+#define VPLUS_BIT PIND7
+
+#define \
+    HOLD(X) \
+    hold(X)
+
+#define \
+    hold(ddr, port, pin) \
+    ({ \
+     ddr |= (1 << pin); \
+     port |= (1 << pin); \
+     _delay_ms(500); \
+     ddr &= ~(1 << pin); \
+     port &= ~(1 << pin); \
+     _delay_ms(1000); \
+    })
+
+
+#define \
+    PRESS(X) \
+    press(X)
+
+#define \
+    press(ddr, port, pin) \
+    ({ \
+     ddr |= (1 << pin); \
+     port |= (1 << pin); \
+     _delay_ms(250); \
+     ddr &= ~(1 << pin); \
+     port &= ~(1 << pin); \
+     _delay_ms(1000); \
+    })
+
