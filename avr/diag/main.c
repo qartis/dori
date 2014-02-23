2059cdb4ef60a2c745d2c0a6cea7a8a83e61868e
Andrew Fuller <qartis@gmail.com>
2014-02-22 16:03:42 -0800

    diag: heating pads automatically controlled via TIMER_TOPHALF and USER_IRQ

--- avr/diag/main.c
+++ avr/diag/main.c
@@ -22,14 +22,75 @@
 #include "power.h"
 #include "temp.h"
 
+void timer_tophalf(void)
+{
+    irq_signal |= IRQ_USER;
+}
+
+uint8_t user_irq(void)
+{
+    uint8_t rc;
+    int16_t temp;
+    static uint8_t seconds = 0;
+
+    seconds++;
+
+    if (seconds < 3)
+        return 0;
+
+    seconds = 0;
+
+    uint16_t adc_voltage = get_voltage();
+    uint16_t mV;
+    uint32_t V;
+
+    V = adc_voltage * 50000; /* 5.00V */
+    V >>= 10;
+    V += 100000;
+    mV = V % 10000;
+    V = V / 10000;
+
+    printf("%lu.%uV (%u)\n", V, mV, adc_voltage);
+
+    if (adc_voltage < 220) {
+        printf("voltage too low, disabling heaters\n");
+        heater_off(0);
+        heater_off(1);
+        heater_off(2);
+        heater_off(3);
+        return 0;
+    }
+
+
+    uint16_t current = get_current();
+    printf("current: %u\n", current);
+
+    rc = temp_begin();
+    if (rc)
+        return 0;
+
+    temp_wait();
+
+    uint8_t i;
+
+    for (i = 0; i < temp_num_sensors; i++) {
+        rc = temp_read(i, &temp);
+        if (rc)
+            return 0;
+
+        if (temp < 170)
+            heater_on(i);
+        else
+            heater_off(i);
+    }
+
+    return 0;
+}
+
 uint8_t send_temp_can(int8_t index, uint8_t type)
 {
     uint8_t buf[2];
     uint8_t rc;
-
-    if (num_sensors == 0) {
-        temp_init();
-    }
 
     temp_begin();
     temp_wait();
@@ -39,36 +100,34 @@ uint8_t send_temp_can(int8_t index, uint8_t type)
     // -1 means send all temperature sensor readings
     if (index == -1) {
         uint8_t i = 0;
-        for (i = 0; i < num_sensors; i++) {
+        for (i = 0; i < temp_num_sensors; i++) {
             rc = temp_read(i, &temp);
             if (rc) {
                 rc = mcp2515_send_wait(
-                        TYPE_sensor_error, MY_ID, 
-                        NULL, 0, SENSOR_temp0 + i);
+                        TYPE_sensor_error, MY_ID,
+                        NULL, 0, SENSOR_temp5 + i);
                 /* what should we do here? sensor
                 measurement screwed up, but we
                 reported it successfully. */
                 return rc;
             }
+
             buf[0] = temp >> 8;
             buf[1] = temp & 0xFF;
 
             printf("temp: %d\n", temp);
 
             rc = mcp2515_send_wait(type, MY_ID,
-                    buf, 2, SENSOR_temp0 + i);
-            if (rc) {
+                    buf, 2, SENSOR_temp5 + i);
+            if (rc)
                 return rc;
-            }
-
-            _delay_ms(150);
         }
     } else {
         rc = temp_read(index, &temp);
         if (rc) {
             rc = mcp2515_send_wait(TYPE_sensor_error,
                     MY_ID, NULL, 0, 
-                    SENSOR_temp0 + index);
+                    SENSOR_temp5 + index);
             /* what should we do here? sensor
                measurement screwed up, but we
                reported it successfully. */
@@ -79,7 +138,7 @@ uint8_t send_temp_can(int8_t index, uint8_t type)
         buf[1] = temp & 0xFF;
 
         rc = mcp2515_send_wait(type, MY_ID, buf,
-                2, SENSOR_temp0 + index);
+                2, SENSOR_temp5 + index);
         if (rc) {
             return rc;
         }
@@ -148,10 +207,13 @@ uint8_t send_all_can(uint8_t type)
     return rc;
 }
 
-
 uint8_t periodic_irq(void)
 {
     uint8_t rc;
+
+    printf("hey\n");
+
+    return 0;
 
     // -1 means send all temperature sensor readings
     rc = send_temp_can(-1, TYPE_value_periodic);
@@ -185,7 +247,7 @@ uint8_t can_irq(void)
     case TYPE_value_request:
         switch(packet.sensor) {
         case SENSOR_temp5 ... SENSOR_temp8:
-            send_temp_can(packet.sensor - SENSOR_temp0,
+            send_temp_can(packet.sensor - SENSOR_temp5,
                           TYPE_value_explicit);
             break;
         case SENSOR_voltage:
@@ -216,8 +278,29 @@ uint8_t uart_irq(void)
     buf[strlen(buf) - 1] = '\0';
 
     if (buf[0] == '\0') {
+        send_temp_can(-1, 0);
         uint16_t current = get_current();
         printf("I: %u\n", current);
+
+
+        uint16_t adc_voltage = get_voltage();
+        uint16_t mV;
+        uint32_t V;
+
+        V = adc_voltage * 53500; /* 5.00V */
+        V >>= 10;
+        V += 100000;
+        mV = V % 10000;
+        V = V / 10000;
+
+        buf[0] = adc_voltage >> 8;
+        buf[1] = (adc_voltage & 0x00FF);
+
+        printf("voltage: %lu.%uV (%u)\n", V, mV, 
+                adc_voltage);
+
+
+
     } else {
         heater_on(0);
         heater_on(1);
@@ -241,7 +324,7 @@ uint8_t uart_irq(void)
     return 0;
 }
 
-void main(void)
+int main(void)
 {
     NODE_INIT();
 
