0431eb4c4b523fd47d11d60018630b90777484ff
Andrew Fuller <qartis@gmail.com>
2014-02-25 18:48:00 -0800

    decan: add pretty printing for SENSOR_{current,temp,time}

--- tests/decan.c
+++ tests/decan.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "../can.h"
 #include "../can_names.h"
@@ -23,7 +24,7 @@ void decan(int type, int id, int sensor, int len, uint8_t *data)
 {
     int i;
 
-    printf("Frame: %s[%x] %s[%x].%s[%x] ", type_names[type], type,
+    printf("Frame: %s[%02x] %s[%02x].%s[%02x] ", type_names[type], type,
         id_names[id], id, sensor_names[sensor], sensor);
 
     if (type == TYPE_value_explicit && id == ID_enviro && sensor ==
@@ -34,25 +35,37 @@ void decan(int type, int id, int sensor, int len, uint8_t *data)
                        data[3] << 0;
 
         printf("%u pascals\n", pressure);
-
-        return;
     } else if (type == TYPE_value_explicit && id == ID_diag && sensor ==
             SENSOR_voltage) {
         int num = (data[0] << 8) | data[1];
         data[7] = '\0';
         printf("0x%02x (%sV)\n", num, data + 2);
+    } else if (type == TYPE_value_explicit && id == ID_diag && sensor ==
+            SENSOR_current) {
+        int num = (data[0] << 8) | data[1];
+        printf("%umA\n", num);
+    } else if ((type == TYPE_value_explicit || type == TYPE_value_periodic) &&
+            sensor >= SENSOR_temp0 && sensor <= SENSOR_temp8) {
+        int temp = (data[0] << 8) | data[1];
+        printf("%u.%u°C\n", temp / 10, temp % 10);
+    } else if ((type == TYPE_value_explicit || type == TYPE_value_periodic) &&
+            sensor == SENSOR_time) {
+        time_t time =
+            (data[0] << 24) |
+            (data[1] << 16) |
+            (data[2] << 8)  |
+            (data[3] << 0);
+        printf("%s\n", ctime(&time));
+    } else {
+        if (len > 0)
+            printf("%d:", len);
 
-        return;
+        for (i = 0; i < len; i++) {
+            printf(" %02x", data[i]);
+        }
+
+        printf("\n");
     }
-
-    if (len > 0)
-        printf("%d:", len);
-
-    for (i = 0; i < len; i++) {
-        printf(" %x", data[i]);
-    }
-
-    printf("\n");
 }
 
 int main(void)
