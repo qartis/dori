d6c912780bb9238451b327b574662b660203f804
Andrew Fuller <qartis@gmail.com>
2014-03-03 19:50:21 -0800

    temp.c: reduce debug message lengths

--- avr/temp.c
+++ avr/temp.c
@@ -10,7 +10,7 @@ uint8_t temp_num_sensors;
 void temp_init(void)
 {
     temp_num_sensors = search_sensors();
-    printf_P(PSTR("temp: %d found\n"), temp_num_sensors);
+    printf_P(PSTR("temp: [%d]\n"), temp_num_sensors);
 }
 
 uint8_t temp_begin(void)
@@ -24,7 +24,7 @@ uint8_t temp_begin(void)
     }
 
     if (ds18b20_start_meas(DS18B20_POWER_PARASITE, NULL)) {
-        puts_P(PSTR("temp: short circuit"));
+        puts_P(PSTR("temp: er34"));
         temp_num_sensors = 0;
         return 1;
     }
@@ -40,12 +40,12 @@ void temp_wait(void)
 uint8_t temp_read(uint8_t channel, int16_t *temp)
 {
     if (ds18b20_read_decicelsius(&sensor_ids[channel][0], temp)) {
-        puts_P(PSTR("temp: crc err"));
+        puts_P(PSTR("temp: crc!"));
         temp_num_sensors = 0;
         return 1;
     }
 
-    printf_P(PSTR("temp %d: %d\n"), channel, *temp);
+//    printf_P(PSTR("temp %d: %d\n"), channel, *temp);
 
     return 0;
 }
