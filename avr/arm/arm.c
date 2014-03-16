4b63227cea70e1f5f47f6d122087b36b2d1dbb3f
Andrew Fuller <qartis@gmail.com>
2014-03-16 14:38:14 -0700

    arm: add timeout when moving arm

--- avr/arm/arm.c
+++ avr/arm/arm.c
@@ -6,6 +6,13 @@
 #include "arm.h"
 #include "adc.h"
 
+void arm_init(void)
+{
+    /* C2: set HI to measure arm */
+    DDRC |= (1 << PORTC2);
+    PORTC |= (1 << PORTC2);
+}
+
 uint16_t get_arm_angle(void)
 {
     uint16_t val;
@@ -15,28 +22,48 @@ uint16_t get_arm_angle(void)
     return val;
 }
 
-void set_arm_percent(uint8_t pos)
+uint8_t set_arm_percent(uint8_t pos)
 {
-    DDRD |= (1 << PORTD2) | (1 << PORTD7);
+    uint16_t goal;
+    uint16_t now;
+    uint8_t retry;
 
     /* pos is 0..9 */
-    uint16_t goal = map(pos, 0, 9, 530, 1000);
+    goal = map(pos, 0, 9, 530, 1000);
+    now = get_arm_angle();
 
-    uint16_t now = get_arm_angle();
+    DDRD |= (1 << PORTD2) | (1 << PORTD7);
+
     printf_P(PSTR("goal: %d"), goal);
     printf_P(PSTR("now: %d"), now);
+
     if ((now + 20) > goal && (now - 20) < goal) {
         puts_P(PSTR("done"));
-        //nothing
+
+        return 0;
     } else if (now > goal) {
-        printf("d2\n");
-        PORTD |=  (1 << PORTD2);
-        while (get_arm_angle() > goal) { _delay_ms(10); };
+        PORTD |= (1 << PORTD2);
+
+        /* 255 * 30 = 7650 ms */
+        retry = 255;
+        while (get_arm_angle() > goal && --retry) {
+            _delay_ms(30);
+        }
+
         PORTD &= ~(1 << PORTD2);
+
+        return !(get_arm_angle() > goal);
     } else {
-        printf("d7\n");
-        PORTD |=  (1 << PORTD7);
-        while (get_arm_angle() < goal) { _delay_ms(10); };
+        PORTD |= (1 << PORTD7);
+
+        /* 255 * 30 = 7650 ms */
+        retry = 255;
+        while (get_arm_angle() < goal && --retry) {
+            _delay_ms(30);
+        }
+
         PORTD &= ~(1 << PORTD7);
+
+        return !(get_arm_angle() > goal);
     }
 }
