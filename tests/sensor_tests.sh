c15ede6fabbbfbcbd72e99b426fbd55fd1d57b44
vedran <vedranb@gmail.com>
2014-03-28 22:53:29 -0700

    Fixed error in sensor tests script

--- tests/sensor_tests.sh
+++ tests/sensor_tests.sh
@@ -1,56 +1,79 @@
 #9DOF
 echo "../shell/send value_request 9dof.gyro"
+../shell/send value_request 9dof.gyro
 sleep 5
 echo "../shell/send value_request 9dof.accel"
+../shell/send value_request 9dof.accel
 sleep 5
 echo "../shell/send value_request 9dof.sats"
+../shell/send value_request 9dof.sats
 sleep 5
 echo "../shell/send value_request 9dof.compass"
+../shell/send value_request 9dof.compass
 sleep 5
 echo "../shell/send value_request 9dof.coords"
+../shell/send value_request 9dof.coords
 sleep 5
 echo "../shell/send value_request 9dof.time"
+../shell/send value_request 9dof.time
 sleep 5
 
 #DIAG
 echo "../shell/send value_request diag.temp5"
+../shell/send value_request diag.temp5
 sleep 5
 echo "../shell/send value_request diag.temp6"
+../shell/send value_request diag.temp6
 sleep 5
 echo "../shell/send value_request diag.temp7"
+../shell/send value_request diag.temp7
 sleep 5
 echo "../shell/send value_request diag.temp8"
+../shell/send value_request diag.temp8
 sleep 5
 echo "../shell/send value_request diag.voltage"
+../shell/send value_request diag.voltage
 sleep 5
 echo "../shell/send value_request diag.current"
+../shell/send value_request diag.current
 sleep 5
 
 #ENVIRO
 echo "../shell/send value_request enviro.temp1"
+../shell/send value_request enviro.temp1
 sleep 5
 echo "../shell/send value_request enviro.temp2"
+../shell/send value_request enviro.temp2
 sleep 5
 echo "../shell/send value_request enviro.rain"
+../shell/send value_request enviro.rain
 sleep 5
 echo "../shell/send value_request enviro.wind"
+../shell/send value_request enviro.wind
 sleep 5
 echo "../shell/send value_request enviro.humidity"
+../shell/send value_request enviro.humidity
 sleep 5
 echo "../shell/send value_request enviro.pressure"
+../shell/send value_request enviro.pressure
 sleep 5
 
 #ARM
-echo "../send value_request arm.arm"
+echo "../shell/send value_request arm.arm"
+../shell/send value_request arm.arm
 sleep 5
-echo "../send value_request arm.laser"
+echo "../shell/send value_request arm.laser"
+../shell/send value_request arm.laser
 sleep 10 # extra long
-echo "../send value_request arm.stepper"
+echo "../shell/send value_request arm.stepper"
+../shell/send value_request arm.stepper
 sleep 5
 
 #MODEMA
-echo "../send value_request modema.uptime"
+echo "../shell/send value_request modema.uptime"
+../shell/send value_request modema.uptime
 sleep 5
 
 #MODEMB
-echo "../send value_request modemb.uptime"
+echo "../shell/send value_request modemb.uptime"
+../shell/send value_request modemb.uptime
