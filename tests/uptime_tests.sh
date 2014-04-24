96b74408eb3864380623949e9d1faf290abb7491
Andrew Fuller <qartis@gmail.com>
2014-04-23 22:55:22 -0700

    tests: change to 5 second delay

--- tests/uptime_tests.sh
+++ tests/uptime_tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
-send value_request 9dof.uptime; sleep 3
-send value_request arm.uptime; sleep 3
-send value_request cam.uptime; sleep 3
-send value_request diag.uptime; sleep 3
-send value_request drive.uptime; sleep 3
-send value_request enviro.uptime; sleep 3
-send value_request modema.uptime; sleep 3
-send sos_nostfu modemb; sleep 3
-send value_request modemb.uptime
+send value_request 9dof.uptime; sleep 5
+send value_request arm.uptime; sleep 5
+send value_request cam.uptime; sleep 5
+send value_request diag.uptime; sleep 5
+send value_request drive.uptime; sleep 5
+send value_request enviro.uptime; sleep 5
+send value_request modema.uptime; sleep 5
+send sos_nostfu modemb; sleep 10
+send value_request modemb.uptime; sleep 5
+send sos_stfu modemb; sleep 5
