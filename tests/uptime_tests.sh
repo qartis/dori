39dcf9864942e7ec6a4f48e70176fcdb514c6846
Andrew Fuller <qartis@gmail.com>
2014-05-21 12:47:04 -0700

    shell: uptime_tests now delays 1s between requests

--- tests/uptime_tests.sh
+++ tests/uptime_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
-send value_request 9dof.uptime; sleep 5
-send value_request arm.uptime; sleep 5
-send value_request cam.uptime; sleep 5
-send value_request diag.uptime; sleep 5
-send value_request drive.uptime; sleep 5
-send value_request enviro.uptime; sleep 5
-send value_request modema.uptime; sleep 5
+send value_request 9dof.uptime; sleep 1
+send value_request arm.uptime; sleep 1
+send value_request cam.uptime; sleep 1
+send value_request diag.uptime; sleep 1
+send value_request drive.uptime; sleep 1
+send value_request enviro.uptime; sleep 1
+send value_request modema.uptime; sleep 1
 send sos_nostfu modemb; sleep 10
-send value_request modemb.uptime; sleep 5
-send sos_stfu modemb; sleep 5
+send value_request modemb.uptime; sleep 1
+send sos_stfu modemb; sleep 1
